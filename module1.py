305a4492e1ad06d8d819f2c97ae9268b93789128
jlevar <joshlevar@gmail.com>
2025-08-21 09:44:47 -0600

    version 0.2 (+documentation)

--- module1.py
+++ module1.py
@@ -1,12 +1,30 @@
-# TODO needs pydoc
+"""
+module1.py
+
+A simple module to read and display weather data from a CSV file.
+
+Functions:
+    load_weather_data(file_path: str): Loads CSV weather data
+"""
 
 import pandas as pd
 
-data_path = 'data/Weather Test Data.csv'
- 
-df = pd.read_csv(data_path)
-print(df.head())
+def load_weather_data(file_path: str) -> pd.DataFrame:
+    """
+    Loads weather data from a CSV file and print first 5 rows.
+
+    Parameters:
+    file_path (str): The path to the CSV file containing weather data.
+
+    Returns:
+    pd.DataFrame: A Pandas DataFrame containing the weather data.
+    """
+    df = pd.read_csv(data_path)
+    print(df.head())
+    return df
 
 
 if __name__ == "__main__":
-    print("Module1 executed as the main program")
+    data_path = 'data/Weather Test Data.csv'
+    load_weather_data(data_path)
+ 
